addb4d98e2d7ccaae8966b4395c170f86cf5ffa0
Shwetha A <shwethaadiraj@gmail.com>
2025-10-26 21:05:23 +0530

    add variant and price styling

--- src/components/ShoeCard/ShoeCard.jsx
+++ src/components/ShoeCard/ShoeCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS, WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
+import { formatPrice, pluralize, isNewShoe, formatShoeTag } from '../../utils';
 import Spacer from '../Spacer';
 
 const ShoeCard = ({
@@ -31,16 +31,27 @@ const ShoeCard = ({
       ? 'new-release'
       : 'default'
 
+  const onSale = variant == "on-sale";
+
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant !== "default" && (
+            <VariantLabel
+              style={{
+                "--variant-bg": onSale ? COLORS.primary : COLORS.secondary,
+              }}
+            >
+              {formatShoeTag(variant)}
+            </VariantLabel>
+          )}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price onSale={onSale}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
@@ -57,16 +68,34 @@ const Link = styled.a`
 
 const Wrapper = styled.article``;
 
+const VariantLabel = styled.span`
+  position: absolute;
+  right: -4px;
+  top: 12px;
+  background: purple;
+  font-size: ${14 / 16}rem;
+  font-weight: 700;
+  color: ${COLORS.white};
+  background: var(--variant-bg);
+  padding: 0 10px;
+  border-radius: 2px;
+  line-height: 32px;
+  height: 32px;
+`;
+
 const ImageWrapper = styled.div`
   position: relative;
 `;
 
 const Image = styled.img`
   width: 100%;
+  border-radius: 16px 16px 4px 4px;
 `;
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -74,7 +103,10 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  text-decoration: ${(p) => p.onSale && "line-through"};
+  color: ${(p) => p.onSale && COLORS.gray[700]};
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
